fdc06026a22a0a5b452ce7c834a8309f443e68e2
xeff09 <wongchaita03@gmail.com>
2026-04-20 22:08:04 +0700

    debug: on main

--- main.go
+++ main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/XEFF09/calculator/domain"
+	"github.com/XEFF09/calculator/internal/adapter/local"
+	"github.com/XEFF09/calculator/usecase"
+)
+
 func main() {
+	// mock stock repo
+	stockRepo := local.NewStock()
+
+	order := domain.Order{
+		IsMember: true,
+		Items: map[domain.Item]int{
+			domain.OrangeSet: 7,
+			domain.RedSet:    1,
+		},
+	}
+
+	orderService := usecase.NewOrder(order, stockRepo)
+
+	calculator := usecase.NewCalculator(
+		order,
+		orderService,
+		[]usecase.Promotion{
+			usecase.NewBundlePromotion(stockRepo),
+			usecase.NewMemberPromotion(),
+		},
+	)
+
+	total, err := calculator.Calculate()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Final Price:", total)
 }
